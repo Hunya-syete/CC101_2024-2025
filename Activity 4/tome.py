f09faec8c13fca1a7597a488bd3f1335920777be
claire-crypto <c9114856@gmail.com>
2024-09-10 09:21:45 +0800

    Create tome.py

---
+++ Activity 4/tome.py
@@ -0,0 +1,17 @@
+item = "Samsung Galaxy S22 Ultra "
+cost = 895
+
+SampleText5 = f"The item is {item} and the cost is {cost * 100} pesos"
+print(SampleText5)
+
+strings = ["Animals", "Badger", "Honey Bee", "Honey Badger"]
+
+for string in strings:
+    lowercase_string = string.lower()
+    print(lowercase_string)
+
+strings = ["mavel", "trisha", "claire"]
+
+for string in strings:
+    uppercase_string = string.upper()
+    print(uppercase_string)
